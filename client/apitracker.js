959288c5ca7e73abed83997e14734421f4e79b0d
David Gomes <davidrafagomes@gmail.com>
2015-12-27 19:25:13 +0000

    Dashboard color revamp.

--- client/apitracker.js
+++ client/apitracker.js
@@ -1,9 +1,9 @@
 Template.slack.helpers({
-  slack:function(){
+  slack: function () {
     return RandomCenas.find(
       {
-        api:"slack",
-        msg:{$exists:true}
+        api: "slack",
+        msg: {$exists: true}
       }
     );
   }
@@ -11,49 +11,49 @@ Template.slack.helpers({
 
 
 Template.stats.events({
-  'click #aGithub':function(){
+  'click #aGithub': function () {
     $(".twitter").css('display', 'none');
     $(".facebook").css('display', 'none');
     $(".slack").css('display', 'none');
     $(".github").css('display', 'block');
   },
-  'click #aTwitter':function(){
+  'click #aTwitter': function () {
     $(".twitter").css('display', 'block');
     $(".facebook").css('display', 'none');
     $(".slack").css('display', 'none');
     $(".github").css('display', 'none');
   },
-  'click #aFacebook':function(){
+  'click #aFacebook': function () {
     $(".twitter").css('display', 'none');
     $(".facebook").css('display', 'block');
     $(".slack").css('display', 'none');
     $(".github").css('display', 'none');
   },
-   'click #aSlack':function(){
-      $(".twitter").css('display', 'none');
-      $(".facebook").css('display', 'none');
-      $(".slack").css('display', 'block');
-      $(".github").css('display', 'none');
-    }
+  'click #aSlack': function () {
+    $(".twitter").css('display', 'none');
+    $(".facebook").css('display', 'none');
+    $(".slack").css('display', 'block');
+    $(".github").css('display', 'none');
+  }
 });
 Template.github.events({
-  'click button' : function(){
+  'click button': function () {
     Meteor.call("updateGithub");
   }
 
 });
 Template.twitter.events({
-  'click button' : function(){
+  'click button': function () {
     Meteor.call("updateCenasTwitFol");
   }
 });
 Template.facebook.events({
-  'click button':function(){
+  'click button': function () {
     Meteor.call('updateFacebook');
   }
 });
 Template.slack.events({
-  'click button':function(){
+  'click button': function () {
     Meteor.call('updateSlack');
   }
 })
