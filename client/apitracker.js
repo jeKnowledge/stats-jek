7ef380a1f9e95817d2af5c973d17517165ffb5ad
João Barbosa <Joao@Joaos-MBP-2.home>
2015-12-29 21:30:26 +0000

    Changed collections for github and twitter

--- client/apitracker.js
+++ client/apitracker.js
@@ -1,58 +1,60 @@
 Template.slack.helpers({
+
   slack:function(){
     return SlackCollection.find(
       {
         msg:{$exists:true}
+
       }
-    );
+    )
   }
 });
 
 
 Template.stats.events({
-  'click #aGithub':function(){
+  'click #aGithub': function () {
     $(".twitter").css('display', 'none');
     $(".facebook").css('display', 'none');
     $(".slack").css('display', 'none');
     $(".github").css('display', 'block');
   },
-  'click #aTwitter':function(){
+  'click #aTwitter': function () {
     $(".twitter").css('display', 'block');
     $(".facebook").css('display', 'none');
     $(".slack").css('display', 'none');
     $(".github").css('display', 'none');
   },
-  'click #aFacebook':function(){
+  'click #aFacebook': function () {
     $(".twitter").css('display', 'none');
     $(".facebook").css('display', 'block');
     $(".slack").css('display', 'none');
     $(".github").css('display', 'none');
   },
-   'click #aSlack':function(){
-      $(".twitter").css('display', 'none');
-      $(".facebook").css('display', 'none');
-      $(".slack").css('display', 'block');
-      $(".github").css('display', 'none');
-    }
+  'click #aSlack': function () {
+    $(".twitter").css('display', 'none');
+    $(".facebook").css('display', 'none');
+    $(".slack").css('display', 'block');
+    $(".github").css('display', 'none');
+  }
 });
 Template.github.events({
-  'click button' : function(){
+  'click button': function () {
     Meteor.call("updateGithub");
   }
 
 });
 Template.twitter.events({
-  'click button' : function(){
+  'click button': function () {
     Meteor.call("updateCenasTwitFol");
   }
 });
 Template.facebook.events({
-  'click button':function(){
+  'click button': function () {
     Meteor.call('updateFacebook');
   }
 });
 Template.slack.events({
-  'click button':function(){
+  'click button': function () {
     Meteor.call('updateSlack');
   }
 })
